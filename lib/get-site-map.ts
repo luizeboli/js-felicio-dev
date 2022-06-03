9ace457980f7ce7b5b9be57b10ab7b46b8f8e388
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2022-06-03 20:50:58 -0300

    fix(main): use expiry-map to prevent stale data due memoized function

--- lib/get-site-map.ts
+++ lib/get-site-map.ts
@@ -6,8 +6,10 @@ import { notion } from './notion-api'
 import { getCanonicalPageId } from './get-canonical-page-id'
 import * as config from './config'
 import * as types from './types'
+import ExpiryMap from 'expiry-map'
 
 const uuid = !!includeNotionIdInUrls
+const cache = new ExpiryMap(10000)
 
 export async function getSiteMap(): Promise<types.SiteMap> {
   const partialSiteMap = await getAllPages(
@@ -22,7 +24,8 @@ export async function getSiteMap(): Promise<types.SiteMap> {
 }
 
 const getAllPages = pMemoize(getAllPagesImpl, {
-  cacheKey: (...args) => JSON.stringify(args)
+  cacheKey: (...args) => JSON.stringify(args),
+  cache
 })
 
 async function getAllPagesImpl(
