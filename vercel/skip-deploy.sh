45403c1f76cf2da98dbee6d04b6d9625cb15a0ae
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2022-06-06 14:40:55 -0300

    Fix vercel skip-deploy script

--- vercel/skip-deploy.sh
+++ vercel/skip-deploy.sh
@@ -3,12 +3,11 @@
 echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
 
 if [[ "$VERCEL_GIT_COMMIT_MESSAGE" == "[skip deploy]"* ]] ; then
-  # Proceed with the build
-  echo "✅ - Build can proceed"
-  exit 1;
-
-else
   # Don't build
   echo "🛑 - Build cancelled"
   exit 0;
+else
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
 fi
